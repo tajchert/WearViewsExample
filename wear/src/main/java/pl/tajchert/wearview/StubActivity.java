d335942be96773c6ab3921c6b78e505f2ca0857a
Michal Tajchert <mtajchert@gmail.com>
2014-10-02 23:31:50 +0200

    DismissOverlayView added on long press.

--- wear/src/main/java/pl/tajchert/wearview/StubActivity.java
+++ wear/src/main/java/pl/tajchert/wearview/StubActivity.java
@@ -1,30 +1,41 @@
 package pl.tajchert.wearview;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.wearable.activity.ConfirmationActivity;
+import android.support.wearable.view.DismissOverlayView;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class StubActivity extends Activity {
+public class StubActivity extends Activity implements View.OnLongClickListener{
 
     private TextView mTextView;
+    private DismissOverlayView mDismissOverlayView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stub);
+        mDismissOverlayView = new DismissOverlayView(this);
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
-                Intent intent = new Intent(StubActivity.this, ConfirmationActivity.class);
-                intent.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE, ConfirmationActivity.SUCCESS_ANIMATION);
-                intent.putExtra(ConfirmationActivity.EXTRA_MESSAGE, getString(R.string.success_confirmation));
-                startActivity(intent);
+                stub.setOnLongClickListener(StubActivity.this);
+                stub.addView(mDismissOverlayView,new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.MATCH_PARENT,
+                        RelativeLayout.LayoutParams.MATCH_PARENT));
             }
         });
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        mDismissOverlayView.show();
+        return true;
     }
 }
